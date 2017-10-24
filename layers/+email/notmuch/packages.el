27580abbdbc83046a276963072f652c6d7a1f05c
Francesc Elies Henar <francesc.elies@gmail.com>
2017-10-24 16:44:22 +0200

    [notmuch] binds notmuch-show-previous-message to `C-p`

--- layers/+email/notmuch/packages.el
+++ layers/+email/notmuch/packages.el
@@ -69,6 +69,12 @@
       (evilified-state-evilify-map notmuch-show-mode-map
         :mode notmuch-show-mode
         :bindings
+        ;; In notmuch-show-mode n would be bound to `notmuch-show-next-message`
+        ;; but the evilified state moves the `n' bound function to C-n while
+        ;; it's counterpart `notmuch-show-previous-message` remains bound to
+        ;; `p'. Adding a binding for the previous function to `C-p' becomes
+        ;; handy while navigation messages back and forth.
+        (kbd "C-p")   'notmuch-show-previous-message
         (kbd "o")   'notmuch-show-open-or-close-all
         (kbd "O")   'spacemacs/notmuch-show-close-all
         )
